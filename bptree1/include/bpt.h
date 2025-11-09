8bddf9aed69870d8438922a68a92f84969c7e488
bhj <hhhh8189@naver.com>
2025-11-09 21:44:54 +0900

    find

--- bptree1/include/bpt.h
+++ bptree1/include/bpt.h
@@ -15,45 +15,62 @@
 #define LEAF_MAX 31
 #define INTERNAL_MAX 248
 
-typedef struct record{
-    int64_t key;
-    char value[120];
-}record;
+typedef struct record // leaf 페이지에서 사용되는 실제 데이터 레코드. 총 128 Bytes
+{
+    int64_t key;     // 8 Bytes
+    char value[120]; // 120 Bytes
+} record;
 
-typedef struct inter_record {
-    int64_t key;
-    off_t p_offset;
-}I_R;
+// next_offset = P1 | (K1, P2) | (K2, P3) | (K3, P4) | | | (Kn, Pn+1)
+typedef struct inter_record // internal 페이지에서 사용되는 하위 페이지를 가리키는 인덱스 레코드. 총 16 Bytes
+{
+    int64_t key;    // 8 Bytes
+    off_t p_offset; // 8 Bytes.
+                    // Page Offset(포인터). Key <= 의 데이터가 저장되어 있는 하위 페이지의 디스크 위치(오프셋)를 가리킨다.
+} I_R;
 
-typedef struct Page{
-    off_t parent_page_offset;
-    int is_leaf;
-    int num_of_keys;
-    char reserved[104];
-    off_t next_offset;
-    union{
-        I_R b_f[248];
-        record records[31];
+typedef struct Page // 1 node(page) = 4096 Bytes (4 KB)
+{
+    // 페이지 헤더 (128 Bytes)
+    off_t parent_page_offset; // Parent Page Number: 부모 페이지의 위치(오프셋) 저장. [0, 7].
+                              // + 만약 페이지 구조체가 free 페이지로 사용되는 경우는 다음 free 페이지의 오프셋이 저장된다.
+    int is_leaf;              // 1: leaf page, 0 : internal page. [8, 11]
+    int num_of_keys;          // 현재 페이지에 들어있는 키(레코드)의 개수. [12, 15]
+    char reserved[104];       // [16, 119]
+
+    off_t next_offset; // leaf일 때 : Right Sibling Page Number. [120, 127]
+                       // 디스크 상의 다음 leaf 페이지 위치(오프셋)를 저장해서 leaf 노드를 순차적으로 연결함.
+                       // (가장 오른쪽 리프 페이지는 값이 0)
+                       // internal일 때 : One More Page Number.
+                       // 인덱스 엔트리보다 하나 더 많은 포인터가 필요한 내부 페이지에서,
+                       // 가장 왼쪽 하위 페이지의 위치(오프셋)를 저장하는 데 사용됨.
+
+    // 페이지 바디 (3968 Bytes)
+    union // leaf인지 internal이 정해지면 하나의 역할만 수행
+    {
+        I_R b_f[248];       // internal일 때, 하위 페이지를 가리키는 인덱스 저장. 레코드 총 248개 저장 가능. 248 * 16 = 3968 Bytes
+        record records[31]; // leaf일 때, 실제 데이터 레코드 저장. 레코드 총 31개 저장 가능. 31 * 128 Bytes = 3968 Bytes
     };
-}page;
+} page;
 
-typedef struct Header_Page{
-    off_t fpo;
-    off_t rpo;
-    int64_t num_of_pages;
-    char reserved[4072];
-}H_P;
-
+typedef struct Header_Page // B+ Tree 파일 전체의 메타데이터. 데이터 파일의 첫 번째 페이지이다.
+{
+    off_t fpo;            // 다음에 사용할 수 있는 Free Page의 페이지 번호(offset). Free Page List의 첫 번째 페이지를 가리킨다. [0, 7]
+    off_t rpo;            // Root Page의 페이지 번호(offset). 트리의 시작점이다. [8, 15]
+    int64_t num_of_pages; // 현재 데이터 파일에 할당된 페이지의 개수. [16, 23]
+    char reserved[4072];  // [24, 4095]
+} H_P;
 
 extern int fd;
 
-extern page * rt;
+extern page *rt;
 
-extern H_P * hp;
+extern H_P *hp;
+
 // FUNCTION PROTOTYPES.
-int open_table(char * pathname);
-H_P * load_header(off_t off);
-page * load_page(off_t off);
+int open_table(char *pathname);
+H_P *load_header(off_t off);
+page *load_page(off_t off);
 
 void reset(off_t off);
 off_t new_page();
@@ -62,10 +79,8 @@ int cut(int length);
 int parser();
 void start_new_file(record rec);
 
-char * db_find(int64_t key);
-int db_insert(int64_t key, char * value);
+char *db_find(int64_t key);
+int db_insert(int64_t key, char *value);
 int db_delete(int64_t key);
 
 #endif /* __BPT_H__*/
-
-
--- bptree1/src/bpt.c
+++ bptree1/src/bpt.c
@@ -1,172 +1,241 @@
 #include "bpt.h"
 
-H_P * hp;
+H_P *hp;
 
-page * rt = NULL; //root is declared as global
+page *rt = NULL; // root is declared as global
 
-int fd = -1; //fd is declared as global
+int fd = -1; // fd is declared as global. 파일 디스크립터. .db 데이터 파일을 간접적으로 가리키는 논리적인 포인터
 
-
-H_P * load_header(off_t off) {
-    H_P * newhp = (H_P*)calloc(1, sizeof(H_P));
-    if (sizeof(H_P) > pread(fd, newhp, sizeof(H_P), 0)) {
-
+H_P *load_header(off_t off) // 디스크에 있는 B+ Tree 데이터 파일에서 헤더 페이지의 정보를 읽어와 메모리에 로드할 때 사용되는 함수
+{
+    H_P *newhp = (H_P *)calloc(1, sizeof(H_P));
+    if (sizeof(H_P) > pread(fd, newhp, sizeof(H_P), 0)) // 다 읽어오지 못한경우
+    {
+        // pread(int fd, void *buf, size_t count, off_t offset) : 파일 디스크립터를 사용하여 디스크 파일에서 데이터를 읽는다.
+        //                  fd, 읽은 데이터를 저장할 메모리 버퍼, 읽을 최대 바이트 수, .db 파일 내에서 읽기를 시작할 오프셋(위치)
         return NULL;
     }
     return newhp;
 }
 
-
-page * load_page(off_t off) {
-    page* load = (page*)calloc(1, sizeof(page));
-    //if (off % sizeof(page) != 0) printf("load fail : page offset error\n");
-    if (sizeof(page) > pread(fd, load, sizeof(page), off)) {
+page *load_page(off_t off) // 디스크에 있는 B+ Tree 데이터 파일에서 특정 위치(오프셋)에 있는 일반 페이지(노드) 하나를 읽어와 메모리에 로드하는 함수
+{
+    page *load = (page *)calloc(1, sizeof(page));
+    // if (off % sizeof(page) != 0) printf("load fail : page offset error\n");
+    if (sizeof(page) > pread(fd, load, sizeof(page), off))
+    {
 
         return NULL;
     }
     return load;
 }
 
-int open_table(char * pathname) {
-    fd = open(pathname, O_RDWR | O_CREAT | O_EXCL | O_SYNC  , 0775);
+int open_table(char *pathname) // 시작 함수
+{
+    fd = open(pathname, O_RDWR | O_CREAT | O_EXCL | O_SYNC, 0775);
     hp = (H_P *)calloc(1, sizeof(H_P));
-    if (fd > 0) {
-        //printf("New File created\n");
-        hp->fpo = 0;
-        hp->num_of_pages = 1;
-        hp->rpo = 0;
+    if (fd > 0) // 새 파일 생성 및 초기화
+    {
+        // printf("New File created\n");
+        hp->fpo = 0;          // 처음 시작할때는 free 페이지 없음
+        hp->num_of_pages = 1; // 처음 시작할때는 헤더 페이지만 존재
+        hp->rpo = 0;          // 처음 시작할때는 root 페이지 없음
+
         pwrite(fd, hp, sizeof(H_P), 0);
+        // pwrite(int fd, const void *buf, size_t count, off_t offset) : 파일 디스크립터를 사용하여 파일에 데이터를 쓴다.
+        //                  fd, 파일에 쓸 데이터가 저장된 메모리 버퍼, 쓸 바이트 수, .db 파일 내에서 쓰기 시작할 오프셋(위치)
         free(hp);
-        hp = load_header(0);
+        hp = load_header(0); // 방금 디스크에 저장한 최신 정보를 다시 읽어옴
         return 0;
     }
-    fd = open(pathname, O_RDWR|O_SYNC);
-    if (fd > 0) {
-        //printf("Read Existed File\n");
-        if (sizeof(H_P) > pread(fd, hp, sizeof(H_P), 0)) {
+
+    fd = open(pathname, O_RDWR | O_SYNC);
+    if (fd > 0) // 기존 파일 열기 및 로드
+    {
+        // printf("Read Existed File\n");
+        if (sizeof(H_P) > pread(fd, hp, sizeof(H_P), 0))
+        {
             return -1;
         }
         off_t r_o = hp->rpo;
         rt = load_page(r_o);
         return 0;
     }
-    else return -1;
+    else
+        return -1;
 }
 
-void reset(off_t off) {
-    page * reset;
-    reset = (page*)calloc(1, sizeof(page));
+void reset(off_t off) // 디스크의 새로운 공간에 B+ Tree 노드(페이지)로서 필요한 초기 메타데이터를 설정해주는 역할
+{
+    page *reset;
+    reset = (page *)calloc(1, sizeof(page));
     reset->parent_page_offset = 0;
     reset->is_leaf = 0;
     reset->num_of_keys = 0;
     reset->next_offset = 0;
+
     pwrite(fd, reset, sizeof(page), off);
     free(reset);
     return;
 }
 
-void freetouse(off_t fpo) {
-    page * reset;
+void freetouse(off_t fpo) // 이미 파일 내에 확보된 기존 Free Page를 B+ Tree 노드로 재사용하기 위해 초기화
+{
+    page *reset;
     reset = load_page(fpo);
     reset->parent_page_offset = 0;
     reset->is_leaf = 0;
     reset->num_of_keys = 0;
     reset->next_offset = 0;
+
     pwrite(fd, reset, sizeof(page), fpo);
     free(reset);
     return;
 }
 
-void usetofree(off_t wbf) {
-    page * utf = load_page(wbf);
-    utf->parent_page_offset = hp->fpo;
+// B+ Tree에서 더이상 사용하지 않는 노드를 Free Page List의 맨 앞쪽에 연결하여 재사용 할 수 있도록 함. (스택 방식)
+// 이 변경사항을 헤더 페이지에 반영. wbf : want to be free
+void usetofree(off_t wbf)
+{
+    page *utf = load_page(wbf);
+    utf->parent_page_offset = hp->fpo; // utf가 free 페이지 리스트 맨 앞에 옴
     utf->is_leaf = 0;
     utf->num_of_keys = 0;
     utf->next_offset = 0;
-    pwrite(fd, utf, sizeof(page), wbf);
+
+    // 페이지 해제 및 free 페이지 리스트로 write
+    pwrite(fd, utf, sizeof(page), wbf); // fd, 파일에 쓸 데이터가 저장된 메모리 버퍼, 쓸 바이트 수, .db 파일 내에서 쓰기 시작할 오프셋(위치)
     free(utf);
+
+    // Header Page 갱신
     hp->fpo = wbf;
-    pwrite(fd, hp, sizeof(hp), 0);
+    pwrite(fd, hp, sizeof(hp), 0); // 변경된 Header Page를 디스크의 오프셋 0에 새로 기록해서 새로운 Free Page List의 시작점을 반영한다.
     free(hp);
     hp = load_header(0);
     return;
 }
 
-off_t new_page() {
+off_t new_page() // B+ Tree에서 새로운 노드(페이지)가 필요할 때, 디스크 파일 내에 4096 Bytes의 새로운 페이지 공간을 할당하고 초기화하는 역할
+{
     off_t newp;
-    page * np;
+    page *np;
     off_t prev;
-    if (hp->fpo != 0) {
+    if (hp->fpo != 0) // free page가 있다면 재사용
+    {
         newp = hp->fpo;
-        np = load_page(newp);
-        hp->fpo = np->parent_page_offset;
-        pwrite(fd, hp, sizeof(hp), 0);
+        np = load_page(newp); // 메모리로 load
+
+        hp->fpo = np->parent_page_offset; // free 페이지 리스트에서 np 제거
+        pwrite(fd, hp, sizeof(hp), 0);    // 변경된 Header Page 정보를 디스크에 반영
         free(hp);
         hp = load_header(0);
+
         free(np);
         freetouse(newp);
         return newp;
     }
-    //change previous offset to 0 is needed
-    newp = lseek(fd, 0, SEEK_END);
-    //if (newp % sizeof(page) != 0) printf("new page made error : file size error\n");
+
+    // 파일 포인터를 파일의 끝으로 이동시키고, 파일 끝의 새로운 오프셋을 저장
+    newp = lseek(fd, 0, SEEK_END); // lseek(int fd, off_t offset, int whence) : fd를 이동시키는 함수
+                                   // fd, whence 기준으로 이동할 바이트 수, 오프셋 계산 기준점
+
+    // if (newp % sizeof(page) != 0) printf("new page made error : file size error\n");
     reset(newp);
     hp->num_of_pages++;
+
     pwrite(fd, hp, sizeof(H_P), 0);
     free(hp);
     hp = load_header(0);
+
     return newp;
 }
 
-
-
-int cut(int length) {
+int cut(int length)
+{
     if (length % 2 == 0)
         return length / 2;
     else
         return length / 2 + 1;
 }
 
-
-
-void start_new_file(record rec) {
-
-    page * root;
+void start_new_file(record rec) // 디스크 파일 내에 최초의 B+ Tree 구조를 생성하는 역할
+{
+    page *root; // ?
     off_t ro;
     ro = new_page();
     rt = load_page(ro);
     hp->rpo = ro;
-    pwrite(fd, hp, sizeof(H_P), 0);
+
+    pwrite(fd, hp, sizeof(H_P), 0); // hp->rpo가 반영된 새로운 헤더 페이지를 적용
     free(hp);
     hp = load_header(0);
+
     rt->num_of_keys = 1;
     rt->is_leaf = 1;
     rt->records[0] = rec;
+
     pwrite(fd, rt, sizeof(page), hp->rpo);
     free(rt);
     rt = load_page(hp->rpo);
-    //printf("new file is made\n");
+    // printf("new file is made\n");
 }
 
+char *db_find(int64_t key)
+{
+    page *c = load_page(hp->rpo); // root 노드
+    while (c->is_leaf != 1)
+    {
+        off_t next_c = 0;
+        int index = c->num_of_keys;
 
-char * db_find(int64_t key) {
+        int i;
+        for (i = 0; i < c->num_of_keys; i++)
+        {
+            if (key <= c->b_f[i].key) // key보다 크거나 같은 최초의 K i+1를 찾음
+                break;
+        }
 
+        // next_offset = P1 | (K1, P2) | (K2, P3) | (K3, P4) | | | (Kn, Pn+1)
+        // 제일 왼쪽 포인터 P 1는  c->next_offset
+
+        if (i == c->num_of_keys) // key가 모든 값보다 큰 경우. 가장 마지막 포인터로 이동
+            next_c = c->b_f[c->num_of_keys - 1].p_offset;
+        else if (i == 0) // 첫 번째 탐색에서 멈추는 경우
+        {
+            if (key < c->b_f[0].key) // key가 K1보다 작은 경우 P1으로 이동
+                next_c = c->next_offset;
+            else
+                next_c = c->b_f[0].p_offset; // key == k1인 경우
+        }
+        else
+            next_c = c->b_f[i].p_offset;
+
+        // 이전 페이지 메모리 해제
+        page *prev_c = c;
+        c = load_page(next_c);
+        free(prev_c);
+    }
+
+    // leaf 노드 도착
+    char *tmp = NULL;
+    for (int i = 0; i < c->num_of_keys; i++)
+    {
+        if (key == c->records[i].key)
+        {
+            tmp = (char *)malloc(sizeof(c->records[i].value));
+            strcpy(tmp, c->records[i].value);
+            break;
+        }
+    }
+    free(c);
+    return tmp;
 }
 
-int db_insert(int64_t key, char * value) {
-
-
+int db_insert(int64_t key, char *value)
+{
 }
 
+int db_delete(int64_t key)
+{
 
-int db_delete(int64_t key) {
-
-}//fin
-
-
-
-
-
-
-
-
+} // fin
